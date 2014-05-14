dcba3ae352a1bfd34b343660b90768b72d8c820e
Dan Koepke <dan.koepke@appdynamics.com>
2014-05-13 17:43:01 -0700

    Version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='distutils-licenses',
-    version='0.1.0',
+    version='0.1.1',
 
     author='Daniel A. Koepke',
     author_email='dkoepke@gmail.com',
