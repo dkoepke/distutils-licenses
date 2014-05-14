1d83432e79dcd5e03b248fc1c7e0577fcb3cc453
Dan Koepke <dan.koepke@appdynamics.com>
2014-05-13 17:25:42 -0700

    Update docs.

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 distutils-licenses
 ==================
 
-A command for setup.py to print the licenses of installed packages
+A command for setup.py to print the licenses of installed packages.
+
+To use, place in your setup.py:
+
+```python
+setup_requires=['distutils-licenses']
+```
+
+Run:
+
+```python
+python setup.py develop
+```
+
+From then on, you can do:
+
+```python
+python setup.py licenses
+```
--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@ from setuptools import setup
 setup(
     name='distutils-licenses',
     version='0.1.0',
-    description='adds a command to setup.py to list licenses of packages',
+
     author='Daniel A. Koepke',
     author_email='dkoepke@gmail.com',
-    license="MIT",
+    license='MIT',
+
+    description='adds a command to setup.py to list licenses of packages',
+    long_description=open('README.md').read(),
+
+    url='http://github.com/dkoepke/distutils-licenses',
 
     entry_points={
         'distutils.commands': [
